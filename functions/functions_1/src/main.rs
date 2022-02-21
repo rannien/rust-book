9d35ca275bd7c6f1d82a56063df6ef336a6ac245
Csaba Verebélyi <verebelyi.csaba@zengo.ez>
2022-02-21 22:13:35 +0100

    feat: add function chapter examples

---
+++ functions/functions_1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    print_labeled_measurement(5, 'h');
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {}{}", value, unit_label);
+}
